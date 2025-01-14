c1d1d64a811ec3b8f23c6265b4791c17e1d09d29
ashrafsalem2 <eng.ashrafsalem2@gmail.com>
2025-01-15 00:57:20 +0200

    finished portfolio_page

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { InfoComponent } from "../info/info.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [InfoComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+  imageList: string[] = [
+    "/images/portfolio/poert1.png",
+    "/images/portfolio/port2.png",
+    "/images/portfolio/port3.png",
+    "/images/portfolio/poert1.png",
+    "/images/portfolio/port2.png",
+    "/images/portfolio/port3.png"
+  ]
 
+  // control when hover toggle
+  display: boolean = false
+
+  // display layout
+  displayLayout(hoverName: HTMLElement): void {
+
+    let elem = hoverName.querySelector('.layout');
+    elem?.classList.remove('d-none');
+
+  }
+
+  // hide layout
+  hideLayout(hoverName: HTMLElement): void {
+
+    let elem = hoverName.querySelector('.layout');
+    elem?.classList.add('d-none');
+  }
+
+  // image section
+  imageSection: boolean = false;
+  defaultImg: string = "/images/portfolio/poert1.png"
+
+  // toggle to show the image selected
+  toggleSection(imageUrl: string): void {
+    console.log(imageUrl)
+    this.defaultImg = imageUrl
+    this.imageSection = !this.imageSection;
+  }
+
+  // dismiss the layout if i press any where , but the image
+  dismissLayout(selectedImage: any): void {
+    if (selectedImage.target.src === undefined ) {
+      this.imageSection = false
+    }
+
+  }
 }
